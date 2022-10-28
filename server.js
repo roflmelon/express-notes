566e459e5879fec0e37f54debdb8eb8a7eb0a235
roflmelon <harryhz508@gmail.com>
2022-10-28 04:54:03 -0400

    change port

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
-app.listen(PORT, () =>
+app.listen(process.env.PORT || PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
